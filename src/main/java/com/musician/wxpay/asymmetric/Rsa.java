1a8ad1b0cc6d2561f0c541bffb3fbd15584ddc8a
liuxiangren <2945810@qq.com>
2020-12-30 14:27:08 +0800

    非对称加密算法RSA

--- src/main/java/com/musician/wxpay/asymmetric/Rsa.java
+++ src/main/java/com/musician/wxpay/asymmetric/Rsa.java
@@ -1,10 +1,15 @@
 package com.musician.wxpay.asymmetric;
 
+import javax.crypto.Cipher;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.*;
+
 /**
  * @ClassName: Rsa
  * @Description:
  *    DH算法使用到公钥-私钥组成的密钥对，奠定了非对称加密的基础。
- *    -- 对称加密的实现中重要的一环是需要协商密钥,在N个人之间通信的时候，对称加密需要管理N*(N-1)/2个密钥
+ *    -- 对称加密的实现中重要的一环是需要协商密钥
  *    -- 非对称加密可以对外开放自己的公钥
  *
  * @Author: LXR
@@ -12,5 +17,70 @@ package com.musician.wxpay.asymmetric;
  * @Version: 1.0
  */
 public class Rsa {
+    public static void main(String[] args) throws GeneralSecurityException {
+        // 明文:
+        byte[] plain = new byte[0];
+        try {
+            plain = "Hello, encrypt use RSA".getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        // 创建公钥／私钥对:
+        Person alice = new Person("Alice");
+        // 用Alice的公钥加密:
+        byte[] pk = alice.getPublicKey();
+        System.out.println(String.format("public key: %x", new BigInteger(1, pk)));
+        byte[] encrypted = alice.encrypt(plain);
+        System.out.println(String.format("encrypted: %x", new BigInteger(1, encrypted)));
+        // 用Alice的私钥解密:
+        byte[] sk = alice.getPrivateKey();
+        System.out.println(String.format("private key: %x", new BigInteger(1, sk)));
+        byte[] decrypted = alice.decrypt(encrypted);
+        try {
+            System.out.println(new String(decrypted, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+}
+class Person {
+    String name;
+    // 私钥:
+    PrivateKey sk;
+    // 公钥:
+    PublicKey pk;
 
+    public Person(String name) throws GeneralSecurityException {
+        this.name = name;
+        // 生成公钥／私钥对:
+        KeyPairGenerator kpGen = KeyPairGenerator.getInstance("RSA");
+        kpGen.initialize(1024);
+        KeyPair kp = kpGen.generateKeyPair();
+        this.sk = kp.getPrivate();
+        this.pk = kp.getPublic();
+    }
+
+    // 把私钥导出为字节
+    public byte[] getPrivateKey() {
+        return this.sk.getEncoded();
+    }
+
+    // 把公钥导出为字节
+    public byte[] getPublicKey() {
+        return this.pk.getEncoded();
+    }
+
+    // 用公钥加密:
+    public byte[] encrypt(byte[] message) throws GeneralSecurityException {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, this.pk);
+        return cipher.doFinal(message);
+    }
+
+    // 用私钥解密:
+    public byte[] decrypt(byte[] input) throws GeneralSecurityException {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, this.sk);
+        return cipher.doFinal(input);
+    }
 }
