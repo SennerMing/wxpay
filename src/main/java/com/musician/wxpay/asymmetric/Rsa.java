92b4c80d0acb41a98d7d6840478c5e5cfb7b8bc2
liuxiangren <2945810@qq.com>
2020-12-30 14:12:42 +0800

    非对称加密算法RSA

--- src/main/java/com/musician/wxpay/asymmetric/Rsa.java
+++ src/main/java/com/musician/wxpay/asymmetric/Rsa.java
@@ -2,10 +2,15 @@ package com.musician.wxpay.asymmetric;
 
 /**
  * @ClassName: Rsa
- * @Description: TODO
+ * @Description:
+ *    DH算法使用到公钥-私钥组成的密钥对，奠定了非对称加密的基础。
+ *    -- 对称加密的实现中重要的一环是需要协商密钥,在N个人之间通信的时候，对称加密需要管理N*(N-1)/2个密钥
+ *    -- 非对称加密可以对外开放自己的公钥
+ *
  * @Author: LXR
- * @DATE: 2020/12/30 10:59
+ * @since: 2020/12/30 10:59
  * @Version: 1.0
  */
 public class Rsa {
+
 }
